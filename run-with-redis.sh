eab4f08922aa10e6f64c610a14945cef3089f2d8
maybach <289727461@qq.com>
2018-07-13 16:26:31 +0800

    Update run-with-redis.sh
    配置修复

--- run-with-redis.sh
+++ run-with-redis.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 _CTXT=$CATALINA_HOME/conf/context.xml
-sed -i  's#</Context>##' $_CTXT
-echo '<Valve className="com.orangefunction.tomcat.redissessions.RedisSessionHandlerValve" />' >> $_CTXT
-echo '<Manager className="com.orangefunction.tomcat.redissessions.RedisSessionManager"' >> $_CTXT
-echo " host=\"$REDIS_HOST\"" >> $_CTXT
-echo " port=\"$REDIS_PORT\"" >> $_CTXT
-echo " database=\"$REDIS_DB\"" >> $_CTXT
-echo " password=\"$REDIS_PWD\"" >> $_CTXT
-echo ' maxInactiveInterval="60" />' >> $_CTXT
-echo '</Context>' >> $_CTXT
-
+sed -ni "/<Context>/{p;:a;n;H;/<\/Context>/!ba;x;s/.*/<WatchedResource>WEB-INF\/web.xml<\/WatchedResource>\n\
+         <Valve className=\"com.orangefunction.tomcat.redissessions.RedisSessionHandlerValve\" \/>\n\
+         <Manager className=\"com.orangefunction.tomcat.redissessions.RedisSessionManager\"\n\
+         host=\"$REDIS_HOST\"\n\
+         port=\"$REDIS_PORT\"\n\
+         database=\"$REDIS_DB\"\n\
+         password=\"$REDIS_PWD\"\n\
+         maxInactiveInterval=\"$REDIS_TM\" \/>/;G};p" $_CTXT
 exec /run.sh
